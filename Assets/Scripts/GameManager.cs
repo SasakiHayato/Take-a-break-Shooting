ccc7e06a5fad9845f27b38b25f29d6f372d06902
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-01-26 19:10:31 +0900

    GameManagerにGameStateだけ追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Title,
+    InGame,
+    Result,
+}
+
+public class GameManager : MonoBehaviour
+{
+    
+}
